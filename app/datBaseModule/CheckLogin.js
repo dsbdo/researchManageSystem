96aeff9412f4f22f7ed16d15b1ae81a0d0c887cb
dsbdo <primerplus233@gmail.com>
2018-07-10 10:33:37 +0800

    admin add user function realize

--- app/datBaseModule/CheckLogin.js
+++ app/datBaseModule/CheckLogin.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql");
 
 const config = {
-        host : 'localhost',
-        port : 3306,
-        user : 'researchWalker',
-        password : '123456',
-        database : 'researchDatabase',
-        charset : 'UTF8_GENERAL_CI',
-        debug : false
+    host: 'localhost',
+    port: 3306,
+    user: 'researchWalker',
+    password: '123456',
+    database: 'researchDatabase',
+    charset: 'UTF8_GENERAL_CI',
+    debug: false
 }
 function connectStart() {
     var connection = mysql.createConnection(config);
@@ -21,33 +21,93 @@ function connectClose(connection) {
 
 let CheckLogin = {
     //这是一个JSON对象，用以检查登陆是否合法
-    checkAdmin: function( work_id,admin_name, passwd) {
-        let connection = connectStart();
-        connection.query('CALL checkAdmin(\''+work_id+'\',\''+admin_name+'\',\''+passwd+'\','+'@out);', function(err, rows, fields) {
-            if (err) {
-               throw err;
-            }
-            var results = rows[0];
-            var row = results[0];
-            console.log(row.isAdmin?'true is admin' : 'false, it\'s not a admin');
-            return row.isAdmin;
-        });
-        connectClose(connection);
+    checkAdmin: function (work_id, passwd) {
+        let admin_promise = new Promise(function (resolve, reject) {
+            let connection = connectStart();
+            connection.query('CALL checkAdmin(\'' + work_id + '\',\'' + passwd + '\',' + '@out);', function (err, rows, fields) {
+                if (err) {
+                    reject(err);
+                    throw err;
+                }
+                var results = rows[0];
+                var row = results[0];
+                console.log(row.isAdmin ? 'true is admin' : 'false, it\'s not a admin');
+                resolve(row.isAdmin);
+                connectClose(connection);
+            });
+        })
+        return admin_promise;
     },
 
-    checkGeneralUser: function(work_id, passwd) {
-        let connection = connectStart();
-
-        connection.query('CALL checkAdmin(\''+work_id+'\',\''+passwd+'\''+'@out);', function(err, rows, fields) {
-            if (err) {
-               throw err;
-            }
-            var results = rows[0];
-            var row = results[0];
-            console.log(row.isAdmin?'true is a user' : 'false, it\'s not a user');
+    checkGeneralUser: function (work_id, passwd) {
+        let query_promise = new Promise(function (resolve, reject) {
+            let connection = connectStart();
+            connection.query('CALL checkGeneralUser(\'' + work_id + '\',\'' + passwd + '\',' + '@out);', function (err, rows, fields) {
+                if (err) {
+                    reject(err);
+                    throw err;
+                }
+                var results = rows[0];
+                var row = results[0];
+                console.log(row.isValidUser ? 'true is a user' : 'false, it\'s not a user');
+                //这里返回数据，进行resolv
+                resolve(row.isValidUser);
+                connectClose(connection);
+            });
         });
-        connectClose();
-    }
+        return query_promise;
+    },
+    //总共有四种类型的用户--研究院， 秘书， 研究人员，公司, 工作场所
+    //一，admin用户请求添加一个研究院
+    //流程：先向user中添加一个研究院的ID，然后再将这个ID与研究院的其他信息添加进研究院表，其中userID作为研究院的主码
+    adminAddWorkPlace: function(area, place_address, graduate_institute_id){
+        let add_promise = new Promise(function(resolve, reject){
+            let connection = connectStart();
+            connection.query(/*sql statement*/,function(err, rows, fields){
+
+            });
+        });
+        return add_promise;
+    },
+    adminAddInstitute: function(info, secretary_id) {
+        let add_promise = new Promise(function(resolve, reject){
+            let connection = connectStart();
+            connection.query(/*sql statement*/,function(err, rows, fields){
+
+            });
+        });
+        return add_promise;
+    },
+    adminAddSecretary: function(secretary_name, gender, age, employee_time, employee_responsibility){
+        let add_promise = new Promise(function(resolve, reject){
+            let connection = connectStart();
+            connection.query(/*sql statement*/,function(err, rows, fields){
+
+            });
+        });
+        return add_promise;
+    },
+    adminAddResearchPeople: function(research_people_name, gender, age, job_title, research_direction, graduate_institute_id){
+        let add_promise = new Promise(function(resolve, reject){
+            let connection = connectStart();
+            connection.query(/*sql statement*/,function(err, rows, fields){
+
+            });
+        });
+        return add_promise;
+    },
+    adminAddCompany: function(company_name, company_address) {
+        let add_promise = new Promise(function(resolve, reject){
+            let connection = connectStart();
+            connection.query(/*sql statement*/,function(err, rows, fields){
+
+            });
+        });
+        return add_promise;
+    },
+
 }
+
+
 
 module.exports = CheckLogin;
--- app/routes/login.js
+++ app/routes/login.js
@@ -13,12 +13,12 @@ router.post('/admin', function (req, res, next) {
             reason: "empty work_id"
         });
     }
-    if (!req.body.admin_name) {
-        res.status(500).json({
-            result: "error",
-            reason: "empty admin_name"
-        });
-    }
+    // if (!req.body.admin_name) {
+    //     res.status(500).json({
+    //         result: "error",
+    //         reason: "empty admin_name"
+    //     });
+    // }
     if (!req.body.passwd || req.body.passwd.length !== 64) {
         //如果密码为空，或者不是64位，证明这就已经是一个错误的密码了
         res.status(500).json({
@@ -27,17 +27,23 @@ router.post('/admin', function (req, res, next) {
         });
     }
     //调用数据库进行校验
-    let isAdmin = CheckLogin.checkAdmin(req.body.work_id, req.body.admin_name, req.body.passwd);
-    if ( isAdmin ) {
-        //确认是admin用户
-        res.render('index', { title: 'Express' });
-    } else {
-        //不是admin用户
-    }
+    let admin_promise = CheckLogin.checkAdmin(req.body.work_id,  req.body.passwd);
+    admin_promise.then(function(isAdmin){
+        if (isAdmin) {
+            //确认是admin用户
+            res.send("it's a admin");
+        } else {
+            //不是admin用户
+            //这里应该返回重新登陆的界面
+            res.send("it's not a admin")
+        }
+    });
+ 
    
 });
 
 router.post("/generalUser", function(req, res, next){
+    console.log("/generalUser/login")
     if (!req.body.work_id) {
         res.status(500).json({
             result: "error",
@@ -51,13 +57,19 @@ router.post("/generalUser", function(req, res, next){
             reason: "invalid passwd"
         });
     }
+    console.log(req.body.work_id);
+    console.log(req.body.passwd);
+    let query_promise = CheckLogin.checkGeneralUser(req.body.work_id, req.body.passwd);
+    //console.log(isVaildUser);
+    //如何处理错误返回
+    query_promise.then(function(is_valid_user){
+        if(is_valid_user) {
+            res.send("success");
+        } else {
+            res.send("error");
+        }
+    })
 
-    let isVaildUser = CheckLogin.checkGeneralUser(req.body.work_id, req.body.passwd);
-    if(isVaildUser) {
-        //有效用户
-    } else {
-        //用户校验失败
-    }
 });
 
 module.exports = router;
